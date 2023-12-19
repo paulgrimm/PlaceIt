590e2b82fb72bc32e6980f789c77613b37e25bab
Andreas Fuchs <info@anfuchs.de>
2023-12-19 01:16:23 +0100

    updates custom editor buttons, add button to open webserver in browser

--- Assets/Editor/DataManagerEditor.cs
+++ Assets/Editor/DataManagerEditor.cs
@@ -10,22 +10,34 @@ public class DataManagerEditor : Editor
 
         DataManager dataManager = (DataManager)target;
 
-        // Add a custom button to the inspector
-        if (GUILayout.Button("Add Place with Texture"))
-        {
-            dataManager.AddPlaceToDataBase(RESTApiClient.Instance.GetGroupname(), 0.0, 0.0, 0.0, Quaternion.identity, "Demo Place 1", "Demo Info 1", dataManager.GetDemoTexture2D());
-        }
+        EditorGUILayout.Space();
+        EditorGUILayout.LabelField("Test API requests / responses", EditorStyles.boldLabel);
         
         // Add a custom button to the inspector
-        if (GUILayout.Button("Add Place"))
+        if (GUILayout.Button("Add new place"))
         {
             dataManager.AddPlaceToDataBase(RESTApiClient.Instance.GetGroupname(), 0.0, 0.0, 0.0, Quaternion.identity,"Demo Place 2", "Demo Info 2");
         }
         
         // Add a custom button to the inspector
-        if (GUILayout.Button("Request Data from Server"))
+        if (GUILayout.Button("Add new place with texture"))
+        {
+            dataManager.AddPlaceToDataBase(RESTApiClient.Instance.GetGroupname(), 0.0, 0.0, 0.0, Quaternion.identity, "Demo Place 1", "Demo Info 1", dataManager.GetDemoTexture2D());
+        }
+        
+        // Add a custom button to the inspector
+        if (GUILayout.Button("Request all places from server"))
         {
             dataManager.RequestPlacesDataFromServer();
+        }
+        
+        EditorGUILayout.Space();
+        EditorGUILayout.LabelField("API Webinterface", EditorStyles.boldLabel);
+
+        // Add a custom button to the inspector
+        if (GUILayout.Button("Open in browser"))
+        {
+            Application.OpenURL("http://141.100.233.223:3000");
         }
     }
 }
